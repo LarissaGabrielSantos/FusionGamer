ae1be383c85d7fc9b9072b4a918f46cd02c87242
Larissa <larissagsantos077@gmail.com>
2024-06-12 13:44:11 -0300

    Alterações nas páginas existentes, inclusão da página de pagamento do site

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let count = 1;
+/*let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function(){
@@ -14,3 +14,24 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+*/
+//pagamento
+
+document.getElementById('payment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const paymentMethod = document.querySelector('input[name="payment-method"]:checked').value;
+    const name = document.getElementById('name').value;
+    const cardNumber = document.getElementById('card-number').value;
+    const validDate = document.getElementById('valid-date').value;
+    const cvv = document.getElementById('cvv').value;
+
+    console.log('Payment Method:', paymentMethod);
+    console.log('Name:', name);
+    console.log('Card Number:', cardNumber);
+    console.log('Valid Date:', validDate);
+    console.log('CVV:', cvv);
+
+    alert('Pagamento efetuado com sucesso! Retorne a página inicial');
+});
+
